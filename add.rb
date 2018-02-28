149244b0cf43662745b12ad89b2a1aa9ee7efe36
EC2 Default User <ec2-user@ip-172-31-29-237.ap-northeast-1.compute.internal>
2018-02-28 14:10:51 +0000

    2018年  2月 28日 水曜日 14:10:51 UTC

--- add.rb
+++ add.rb
@@ -1,5 +1,22 @@
-
 user = ARGV[0]
 email = ARGV[1]
 
+puts `git remote add #{user} git@github.com-#{user}:#{user}/#{user}.git`
+puts `ssh-keygen -t rsa -C '#{email}' -f ~/.ssh/id_rsa_#{user}`
 
+File.open(File.expand_path('~/.ssh/config'), 'a') do |file|
+  file.puts "
+Host github.com-#{user}
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/id_rsa_#{user}
+  IdentitiesOnly yes
+
+"
+end
+
+File.open(File.expand_path('~/update'), 'a') do |file|
+  file.puts "git push #{user} HEAD:m"
+end
+
+puts File.read('~/.ssh/id_rsa_#{user}.pub')
